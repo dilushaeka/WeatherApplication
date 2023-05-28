6fa8bac37798e1382da76e23d0597cc43678c7f7
dilushaeka <dilushaeka99@gmail.com>
2023-05-28 15:17:52 +0530

    Add Dark And Light Mode

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//=================== Dark mode=================
+$("#btnDark").on("click", () => {
+    $("body").css("backgroundColor", "Black")
+    
+});
+
+
+
+//=================== light mode=================
+$("#btnLight").on("click", () => {
+    $("body").css("backgroundColor", "White")
+    
+});
+
+
+// ===================  fetch data   =============
